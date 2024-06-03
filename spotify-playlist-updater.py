885afb02ec42a7f27f883e3f485ab87bab50b1ca
mousehawk <redjoker.uk@gmail.com>
2024-06-03 16:38:36 +0100

    Update spotify-playlist-updater.py
    Added a counter to show how many times the playlist has been reset...

--- spotify-playlist-updater.py
+++ spotify-playlist-updater.py
@@ -7,7 +7,7 @@ from spotipy.oauth2 import SpotifyOAuth
 
 import base64
 
-print("Starting Playlist updater")
+print("Starting Playlist Updater")
   
 def func():
     scope = 'playlist-modify-public ugc-image-upload'
@@ -19,42 +19,64 @@ def func():
     SPOTIPY_CLIENT_ID = '00000000000000000000000000000000'
     SPOTIPY_CLIENT_SECRET = '00000000000000000000000000000000'
 
-    token = util.prompt_for_user_token(username,scope,SPOTIPY_CLIENT_ID,SPOTIPY_CLIENT_SECRET,redirect_uri='http://your-url.com')
+    token = util.prompt_for_user_token(username,scope,SPOTIPY_CLIENT_ID,SPOTIPY_CLIENT_SECRET,redirect_uri='http://your-url.com',cache_path='tokens.txt')
     sp = spotipy.Spotify(auth=token)
 
     # Playlist Data
     id = 'https://open.spotify.com/playlist/0000000000000000000000'
     results = sp.playlist(id)
     playlist_name = 'Your Playlist Title'
-    playlist_description = 'Your Playlist Description'
-    
-    # Get the time
+    playlist_description = 'Your playlist description'
+
+    # Get the current time
     from datetime import datetime
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
 
-    # Check if playlist has been taken down
+    # Check if the playlist has been reset
     if playlist_name != (results["name"]):
         
-        # If yes, update the details
-        with open("C:\\Users\\Work\\Documents\\Tools\\spotify-playlist-updater-main\\data\\ambient.jpg", "rb") as img_file:
+        # If it has changed, advance the counter
+        f = open('counter.txt', "r")
+        counter = f.readline()
+        f.close()
+        counter = int(counter)
+        counter += int(1)
+        counter = str(counter)
+        g = open('counter.txt', "w")
+        g.write(counter)
+        g.close()
+        
+        # Restore the thumbnail, title & description
+        with open("C:\\Users\\Username\\Path\\To\\thumbnail.jpg", "rb") as img_file:
          myimage = base64.b64encode(img_file.read())
         image_b64 = myimage
-        
         sp.playlist_upload_cover_image(playlist_id=id, image_b64 = myimage)
         sp.user_playlist_change_details(username, id, name=playlist_name, description=playlist_description)
         
-        # Timestamp
-        print(current_time, " - playlist restored")
+        # Show that the playlist was restored for the Xth time
+        print(current_time, " - playlist restored x", counter)
 
     else:
-        # If no, do nothing, just print a timestamp
+        # If it hasn't changed, just show that the playlist was checked
         print(current_time, " - ok")
 
-func()
-schedule.every(5).minutes.do(func)
-  
+
+# Run the script every 1 minutes
 while True:
-    schedule.run_pending()
-    time.sleep(30)
-    continue 
+    try:
+        func()
+        schedule.every(1).minutes.do(func)
+    except:
+        pass
+    else:
+        break
+
+# Keep the script repeating indefinitely
+while True:
+    try:
+        schedule.run_pending()
+        time.sleep(30)
+    except:
+        pass
+
